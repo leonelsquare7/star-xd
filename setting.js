f6bce1082605c5bb109eda3dbcf69695740b47ce
leonelsquare7 <leonelsquare7@gmail.com>
2026-03-22 16:27:35 +0300

    Update owner number and session ID in settings

--- setting.js
+++ setting.js
@@ -3,11 +3,11 @@
 module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
   OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  OWNER_NUMBER: '254702678294', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: king~FgyvxV'', // add your session ID
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
